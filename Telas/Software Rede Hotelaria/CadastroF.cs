a3bcbcfadcaddaf019c7925d532c2c1854523677
Roger <rogerferresouza@gmail.com>
2021-07-19 21:06:16 -0300

    Cadastro de Funcionários conectado ao banco

--- Telas/Software Rede Hotelaria/CadastroF.cs
+++ Telas/Software Rede Hotelaria/CadastroF.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Software_Rede_Hotelaria
 {
@@ -77,10 +78,52 @@ namespace Software_Rede_Hotelaria
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-D83UVNJ\\SQLEXPRESS;Initial Catalog=FUNCIONARIOS;Integrated Security=True");
+
+            string sql = "INSERT INTO DADOS_FUNCIONARIOS(NOME, CPF, RG, SEXO, ESTADO_CIVIL, EMAIL, TELEFONE, ESTADO, CIDADE, RUA, NUMERO, SALARIO, CARGO) VALUES (@NOME, @CPF, @RG, @SEXO, @ESTADO_CIVIL, @EMAIL, @TELEFONE, @ESTADO, @CIDADE, @RUA, @NUMERO, @SALARIO, @CARGO)";
+
+            //Random numeroID = new Random();
+            //numeroID.Next();
+
+            try
+            {
+                SqlCommand c = new SqlCommand(sql, conn);
+
+               
+                c.Parameters.Add(new SqlParameter("@NOME", this.TxtNome.Text));
+                c.Parameters.Add(new SqlParameter("@CPF", this.TxtCpf.Text));
+                c.Parameters.Add(new SqlParameter("@RG", this.TxtRg.Text));
+                c.Parameters.Add(new SqlParameter("@SEXO", this.TxtSexo.Text));
+                c.Parameters.Add(new SqlParameter("@ESTADO_CIVIL", this.TxtEstC.Text));
+                c.Parameters.Add(new SqlParameter("@EMAIL", this.TxtEmail.Text));
+                c.Parameters.Add(new SqlParameter("@TELEFONE", this.TxtTelefone.Text));
+                c.Parameters.Add(new SqlParameter("@ESTADO", this.TxtEstado.Text));
+                c.Parameters.Add(new SqlParameter("@CIDADE", this.TxtCidade.Text));
+                c.Parameters.Add(new SqlParameter("@RUA", this.TxtRua.Text));
+                c.Parameters.Add(new SqlParameter("@NUMERO", this.TxtNum.Text));
+                c.Parameters.Add(new SqlParameter("@SALARIO", this.TxtSal.Text));
+                c.Parameters.Add(new SqlParameter("@CARGO", this.TxtCargo.Text));
+
+                conn.Open();
+
+                c.ExecuteNonQuery();
+
+                conn.Close();
+
             MessageBox.Show("Cadastrado com sucesso!");
             this.Close();
             OpADM admt = new OpADM();
             admt.Show();
+
+            }catch(SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o Erro: " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
