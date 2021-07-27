00317f516691754d1db51a6b5289a0655ba0acc5
Roger <rogerferresouza@gmail.com>
2021-07-26 21:19:25 -0300

    verificação de cpf funcionário

---
+++ Cpf verificado/Software Rede Hotelaria/CadastroC.Designer.cs
@@ -0,0 +1,396 @@
+﻿
+namespace Software_Rede_Hotelaria
+{
+    partial class CadastroC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.TxtNomeC = new System.Windows.Forms.TextBox();
+            this.TxtNomeSC = new System.Windows.Forms.TextBox();
+            this.TxtCpfC = new System.Windows.Forms.TextBox();
+            this.TxtRgC = new System.Windows.Forms.TextBox();
+            this.TxtDtNasc = new System.Windows.Forms.TextBox();
+            this.TxtSexoC = new System.Windows.Forms.TextBox();
+            this.TxtEmailC = new System.Windows.Forms.TextBox();
+            this.TxtTelC = new System.Windows.Forms.TextBox();
+            this.TxtCidadeC = new System.Windows.Forms.TextBox();
+            this.TxtEstadoC = new System.Windows.Forms.TextBox();
+            this.TxtRuaC = new System.Windows.Forms.TextBox();
+            this.TxtNumC = new System.Windows.Forms.TextBox();
+            this.TxtBairroC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "E-mail:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CPF:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "RG:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "DtNasc";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Telefone:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(413, 218);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 15);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Rua:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(390, 252);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 15);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Número:";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(403, 286);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 15);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Bairro:";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(396, 151);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 15);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Cidade:";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(402, 187);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(42, 15);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Estado";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(65, 245);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(35, 15);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Sexo:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(24, 110);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(77, 15);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "Nome Social:";
+            // 
+            // TxtNomeC
+            // 
+            this.TxtNomeC.Location = new System.Drawing.Point(107, 67);
+            this.TxtNomeC.Name = "TxtNomeC";
+            this.TxtNomeC.Size = new System.Drawing.Size(263, 23);
+            this.TxtNomeC.TabIndex = 14;
+            this.TxtNomeC.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // TxtNomeSC
+            // 
+            this.TxtNomeSC.Location = new System.Drawing.Point(107, 106);
+            this.TxtNomeSC.Name = "TxtNomeSC";
+            this.TxtNomeSC.Size = new System.Drawing.Size(263, 23);
+            this.TxtNomeSC.TabIndex = 15;
+            // 
+            // TxtCpfC
+            // 
+            this.TxtCpfC.Location = new System.Drawing.Point(107, 138);
+            this.TxtCpfC.Name = "TxtCpfC";
+            this.TxtCpfC.Size = new System.Drawing.Size(263, 23);
+            this.TxtCpfC.TabIndex = 16;
+            this.TxtCpfC.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // TxtRgC
+            // 
+            this.TxtRgC.Location = new System.Drawing.Point(107, 167);
+            this.TxtRgC.Name = "TxtRgC";
+            this.TxtRgC.Size = new System.Drawing.Size(263, 23);
+            this.TxtRgC.TabIndex = 17;
+            // 
+            // TxtDtNasc
+            // 
+            this.TxtDtNasc.Location = new System.Drawing.Point(106, 202);
+            this.TxtDtNasc.Name = "TxtDtNasc";
+            this.TxtDtNasc.Size = new System.Drawing.Size(263, 23);
+            this.TxtDtNasc.TabIndex = 18;
+            // 
+            // TxtSexoC
+            // 
+            this.TxtSexoC.Location = new System.Drawing.Point(106, 237);
+            this.TxtSexoC.Name = "TxtSexoC";
+            this.TxtSexoC.Size = new System.Drawing.Size(263, 23);
+            this.TxtSexoC.TabIndex = 19;
+            // 
+            // TxtEmailC
+            // 
+            this.TxtEmailC.Location = new System.Drawing.Point(107, 271);
+            this.TxtEmailC.Name = "TxtEmailC";
+            this.TxtEmailC.Size = new System.Drawing.Size(263, 23);
+            this.TxtEmailC.TabIndex = 20;
+            // 
+            // TxtTelC
+            // 
+            this.TxtTelC.Location = new System.Drawing.Point(107, 301);
+            this.TxtTelC.Name = "TxtTelC";
+            this.TxtTelC.Size = new System.Drawing.Size(263, 23);
+            this.TxtTelC.TabIndex = 21;
+            // 
+            // TxtCidadeC
+            // 
+            this.TxtCidadeC.Location = new System.Drawing.Point(449, 143);
+            this.TxtCidadeC.Name = "TxtCidadeC";
+            this.TxtCidadeC.Size = new System.Drawing.Size(331, 23);
+            this.TxtCidadeC.TabIndex = 23;
+            this.TxtCidadeC.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // TxtEstadoC
+            // 
+            this.TxtEstadoC.Location = new System.Drawing.Point(449, 179);
+            this.TxtEstadoC.Name = "TxtEstadoC";
+            this.TxtEstadoC.Size = new System.Drawing.Size(331, 23);
+            this.TxtEstadoC.TabIndex = 24;
+            this.TxtEstadoC.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // TxtRuaC
+            // 
+            this.TxtRuaC.Location = new System.Drawing.Point(449, 208);
+            this.TxtRuaC.Name = "TxtRuaC";
+            this.TxtRuaC.Size = new System.Drawing.Size(331, 23);
+            this.TxtRuaC.TabIndex = 25;
+            this.TxtRuaC.TextChanged += new System.EventHandler(this.textBox12_TextChanged);
+            // 
+            // TxtNumC
+            // 
+            this.TxtNumC.Location = new System.Drawing.Point(449, 244);
+            this.TxtNumC.Name = "TxtNumC";
+            this.TxtNumC.Size = new System.Drawing.Size(331, 23);
+            this.TxtNumC.TabIndex = 26;
+            this.TxtNumC.TextChanged += new System.EventHandler(this.textBox13_TextChanged);
+            // 
+            // TxtBairroC
+            // 
+            this.TxtBairroC.Location = new System.Drawing.Point(449, 278);
+            this.TxtBairroC.Name = "TxtBairroC";
+            this.TxtBairroC.Size = new System.Drawing.Size(331, 23);
+            this.TxtBairroC.TabIndex = 27;
+            this.TxtBairroC.TextChanged += new System.EventHandler(this.textBox14_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(569, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 31);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Software_Rede_Hotelaria.Properties.Resources.Fundo_Laranja;
+            this.pictureBox1.Image = global::Software_Rede_Hotelaria.Properties.Resources.valhallahotel;
+            this.pictureBox1.Location = new System.Drawing.Point(155, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(653, 180);
+            this.pictureBox1.TabIndex = 29;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 22);
+            this.button2.TabIndex = 30;
+            this.button2.Text = "Voltar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CadastroC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Software_Rede_Hotelaria.Properties.Resources.Fundo_Laranja;
+            this.ClientSize = new System.Drawing.Size(820, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtBairroC);
+            this.Controls.Add(this.TxtNumC);
+            this.Controls.Add(this.TxtRuaC);
+            this.Controls.Add(this.TxtEstadoC);
+            this.Controls.Add(this.TxtCidadeC);
+            this.Controls.Add(this.TxtTelC);
+            this.Controls.Add(this.TxtEmailC);
+            this.Controls.Add(this.TxtSexoC);
+            this.Controls.Add(this.TxtDtNasc);
+            this.Controls.Add(this.TxtRgC);
+            this.Controls.Add(this.TxtCpfC);
+            this.Controls.Add(this.TxtNomeSC);
+            this.Controls.Add(this.TxtNomeC);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CadastroC";
+            this.Text = "CadastroC";
+            this.Load += new System.EventHandler(this.CadastroC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox TxtNomeC;
+        private System.Windows.Forms.TextBox TxtNomeSC;
+        private System.Windows.Forms.TextBox TxtCpfC;
+        private System.Windows.Forms.TextBox TxtRgC;
+        private System.Windows.Forms.TextBox TxtDtNasc;
+        private System.Windows.Forms.TextBox TxtSexoC;
+        private System.Windows.Forms.TextBox TxtEmailC;
+        private System.Windows.Forms.TextBox TxtTelC;
+        private System.Windows.Forms.TextBox TxtCidadeC;
+        private System.Windows.Forms.TextBox TxtEstadoC;
+        private System.Windows.Forms.TextBox TxtRuaC;
+        private System.Windows.Forms.TextBox TxtNumC;
+        private System.Windows.Forms.TextBox TxtBairroC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Cpf verificado/Software Rede Hotelaria/CadastroC.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Software_Rede_Hotelaria
+{
+    public partial class CadastroC : Form
+    {
+        public CadastroC()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CadastroC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox14_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-D83UVNJ\\SQLEXPRESS;Initial Catalog=FUNCIONARIOS;Integrated Security=True");
+            string sql = "INSERT INTO CLIENTE(NOME_C, NOMESOCIAL, CPF_C, RG_C, DTNASC_C, SEXO_C, EMAIL_C, TEL_C, CIDADE_C, ESTADO_C, RUA_C, NUM_C, BAIRRO_C) " +
+                "VALUES (@NOME_C, @NOMESOCIAL, @CPF_C, @RG_C, @DTNASC_C, @SEXO_C, @EMAIL_C, @TEL_C, @CIDADE_C, @ESTADO_C, @RUA_C, @NUM_C, @BAIRRO_C)";
+
+            try
+            {
+                SqlCommand c = new SqlCommand(sql, conn);
+
+                c.Parameters.Add(new SqlParameter("@NOME_C", this.TxtNomeC.Text));
+                c.Parameters.Add(new SqlParameter("@NOMESOCIAL", this.TxtNomeSC.Text));
+                c.Parameters.Add(new SqlParameter("@CPF_C", this.TxtCpfC.Text));
+                c.Parameters.Add(new SqlParameter("@RG_C", this.TxtRgC.Text));
+                c.Parameters.Add(new SqlParameter("@DTNASC_C", this.TxtDtNasc.Text));
+                c.Parameters.Add(new SqlParameter("@SEXO_C", this.TxtSexoC.Text));
+                c.Parameters.Add(new SqlParameter("@EMAIL_C", this.TxtEmailC.Text));
+                c.Parameters.Add(new SqlParameter("@TEL_C", this.TxtTelC.Text));
+                c.Parameters.Add(new SqlParameter("@CIDADE_C", this.TxtCidadeC.Text));
+                c.Parameters.Add(new SqlParameter("@ESTADO_C", this.TxtEstadoC.Text));
+                c.Parameters.Add(new SqlParameter("@RUA_C", this.TxtRuaC.Text));
+                c.Parameters.Add(new SqlParameter("@NUM_C", this.TxtNumC.Text));
+                c.Parameters.Add(new SqlParameter("@BAIRRO_C", this.TxtBairroC.Text));
+
+                conn.Open();
+                c.ExecuteNonQuery();
+                conn.Close();
+
+
+                MessageBox.Show("Cadastrado com sucesso!");
+                this.Close();
+                Home voltar = new Home();
+                voltar.Show();
+                
+            } catch(SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro: " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Home voltar = new Home();
+            voltar.Show();
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
